b0071fadeae75045a898f0dbc5a36464b71c6ead
yenww <yanww0226@163.com>
2021-05-19 11:08:47 +0800

    0521页面导航栏

---
+++ custom-tab-bar/index.js
@@ -0,0 +1,52 @@
+Component({
+  data: { 
+    selected: 0,
+    list: [
+      {
+        pagePath: "/pages/index/index",
+        text: "首页",
+        normal:'/image/tabber/home.png',
+        active:'/image/tabber/home-active.png'
+      },
+      {
+        pagePath: "/pages/mall/mall",
+        text: "商城",
+        normal:'/image/tabber/mall.png',
+        active:'/image/tabber/mall-active.png'
+      },
+      {
+        pagePath:"/pages/live/live",
+        text:"直播",
+        normal:'/image/tabber/live.png',
+        active:'/image/tabber/live-active.png'
+      },
+      {
+        pagePath:"/pages/cart/cart",
+        text:"购物车",
+        normal:'/image/tabber/cart.png',
+        active:'/image/tabber/cart-active.png'
+      },
+      {
+        pagePath:"/pages/mine/mine",
+        text:"我的",
+        normal:'/image/tabber/mine.png',
+        active:'/image/tabber/mine-active.png'
+      }
+    ]
+  },
+  methods: {
+    onChange(e) {
+       console.log(e,'e')
+       this.setData({ active: e.detail });
+       wx.switchTab({
+         url: this.data.list[e.detail].pagePath
+       });
+    },
+    init() {
+        const page = getCurrentPages().pop();
+        this.setData({
+       　  active: this.data.list.findIndex(item => item.pagePath === `/${page.route}`)
+        });
+       }
+    }
+})
--- pages/index/index.js
+++ pages/index/index.js
@@ -3,37 +3,67 @@
 const app = getApp()
 
 Page({
-  data: {
-    active: 0,
-  },
-   onChange(event) {
-    var index = event.detail
 
-    if (index == 0) {
-      that.data.active = 0
-    }
-    else if (index == 1) {
-      wx.navigateTo({
-        url: '../mall/mall',
-      })
-    } 
-    else if (index == 2) {
-      wx.navigateTo({
-        url: '../live/live',
-      })
-    } 
-    else if (index == 3) {
-      wx.navigateTo({
-        url: '../cart/cart',
-      })
-    } 
-    else if (index == 4) {
-      wx.redirectTo({
-        url: '../mine/mine',
-      })
-    }
-    this.setData({
-      active: that.data.active
-    });
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
   },
-});
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    this.getTabBar().init();
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
