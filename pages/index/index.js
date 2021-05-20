fc53c6a61625ca2a63a90d89410105e7ae3082ca
yenww <yanww0226@163.com>
2021-05-20 10:54:07 +0800

    初始化

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,9 +8,24 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    swiperList:[
+      'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.mp.itc.cn%2Fupload%2F20161205%2F7f4cf70807bc4fcfb76356be2a947541_th.jpeg&refer=http%3A%2F%2Fimg.mp.itc.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1624007866&t=c601ed68225ae0285cfd1992729232e7'
+    ],
+    picList:[
+      "/image/grid/shangjia.png",
+      "/image/grid/hehuo.png"
+    ],
+    hei:""
   },
-
+  imgH: function (e) {
+    let winWid = wx.getSystemInfoSync().windowWidth;         
+    let imgh = e.detail.height;　　　　　　　　　　　　　　　
+    let imgw = e.detail.width;
+    let swiperH = winWid * imgh / imgw + "px"
+    this.setData({
+      hei: swiperH　　　　　　　　
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
