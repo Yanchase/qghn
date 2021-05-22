d5c4a4b8cd73a0283b691d2721c8d9ad6eafb991
Fea1n <60054083+Fea1n@users.noreply.github.com>
2021-05-22 09:08:42 +0800

    个人界面更新

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -2,6 +2,23 @@
 Page({
   data: {
        userinfo:{},
+       hasUserInfo: false,
+       canIUse: wx.canIUse('button.open-type.getUserInfo'),
+       canIUseGetUserProfile: false,
+       canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName')
+  },
+  getUserProfile(e) {
+ 
+    wx.getUserProfile({
+      desc: '展示用户信息', 
+      success: (res) => {
+        console.log(res)
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    })
   },
    onShow()
    {
